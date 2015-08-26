e6e8c246c0d0912341c4b378db2de8bebfe229eb
phyng <phyngk@gmail.com>
2015-08-26 21:24:13 +0800

    Update tree.py

--- chapter6/tree.py
+++ chapter6/tree.py
@@ -12,25 +12,43 @@ class Node(object):
         self.left = None
         self.right = None
 
+    def add(self, word):
+        if word == self.word:
+            self.count += 1
+        elif word < self.word:
+            if self.left:
+                self.left.add(word)
+            else:
+                self.left = Node(word)
+        elif word > self.word:
+            if self.right:
+                self.right.add(word)
+            else:
+                self.right = Node(word)
+
+    def add_words(self, words):
+        for word in words:
+            self.add(word)
+
+    def search(self, word):
+        if word == self.word:
+            return self
+        elif word < self.word and self.left:
+            return self.left.search(word)
+        elif word > self.word and self.right:
+            return self.right.search(word)
+        return None
+
+    def items(self):
+        return {
+            'word': self.word,
+            'count': self.count,
+            'left': self.left.items() if self.left else None,
+            'right': self.right.items() if self.right else None,
+        }
+
     def __str__(self):
-        return '{{"word": "{}", "count": {}, "left": {}, "right": {}}}'.format(
-            self.word, self.count, str(self.left or 'null'), str(self.right or 'null'))
-
-
-def add_to_tree(root_node, word):
-
-    if word == root_node.word:
-        root_node.count += 1
-    elif word < root_node.word:
-        if root_node.left:
-            add_to_tree(root_node.left, word)
-        else:
-            root_node.left = Node(word)
-    elif word > root_node.word:
-        if root_node.right:
-            add_to_tree(root_node.right, word)
-        else:
-            root_node.right = Node(word)
+        return json.dumps(self.items(), indent=2)
 
 
 def main():
@@ -41,10 +59,11 @@ def main():
     words = get_article().split()
     shuffle(words)
 
+    a = words.pop()
     root = Node(words.pop())
-    for word in words:
-        add_to_tree(root, word)
-    print json.dumps(json.loads(str(root)), indent=2)
+    root.add_words(words)
+    print root
+    print root.search(a)
 
 
 if __name__ == '__main__':
